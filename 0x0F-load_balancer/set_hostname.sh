48fa356f42308cd171b479f2c0e9b1306bbc693b
ochuks1 <cochuks@gmail.com>
2024-08-26 07:02:29 -0700

    corrections

--- 0x0F-load_balancer/set_hostname.sh
+++ 0x0F-load_balancer/set_hostname.sh
@@ -2,17 +2,17 @@
 # This script sets the hostname and updates the /etc/hosts file.
 
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <ochuks1>"
+    echo "Usage: $0 <web-02>"
     exit 1
 fi
 
 NEW_HOSTNAME=$1
 
 # Set the hostname
-echo "ochuks" > /etc/hostname
-hostname "ochuks"
+echo "web-01" > /etc/hostname
+hostname "web-01"
 
 # Update /etc/hosts
-sed -i "/127.0.1.1/c\127.0.1.1\tochuks" /etc/hosts
+sed -i "/127.0.1.1/c\127.0.1.1\tweb-01" /etc/hosts
 
-echo "Hostname set to ochuks"
+echo "Hostname set to web-01"
