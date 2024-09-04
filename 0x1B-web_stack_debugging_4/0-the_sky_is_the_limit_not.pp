4ba6a199cf973d94803461f2b01931a08d9e2374
ochuks1 <cochuks@gmail.com>
2024-09-04 15:35:16 -0700

    Tasks

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,3 +5,15 @@ exec { 'fix--for-nginx':
   command => '/path/to/nginx/configure-fix.sh',
   path    => ['/usr/local/bin', '/usr/bin', '/bin'],
 }
+
+file { '/etc/nginx/nginx.conf':
+  ensure  => file,
+  source  => 'puppet:///modules/default/nginx.conf',
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/nginx.conf'],
+}
