4354baa69650f0baceb5e0ed0fe000e7340add8a
ochuks1 <cochuks@gmail.com>
2024-09-04 15:49:30 -0700

    Taskss

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -6,7 +6,7 @@
 exec { 'increase-nginx-ulimit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin:/bin',
-} ->
+}
 
 # Restart Nginx to apply the changes
 exec { 'restart-nginx':
