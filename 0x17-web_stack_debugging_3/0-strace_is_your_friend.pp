5839f3c262394b8dd82ac67d37526d87fdbc454a
ochuks1 <cochuks@gmail.com>
2024-09-04 13:44:40 -0700

    Correction

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,19 +1,9 @@
 # Puppet Manifest to fix Apache 500 Internal Server Error
 # Description: Fixing issue identified with strace where Apache returns 500 error
 
-# Ensure the index.php file exists with correct permissions
-file { '/var/www/html/index.php':
-  ensure => file,
-  source => 'puppet:///modules/my_module/index.php',
-  mode   => '0644',
-  owner  => 'www-data',
-  group  => 'www-data',
-}
 
-# Restart Apache to apply changes
-service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/var/www/html/index.php'],
+exec { 'fix-wordpress':
+  command => '/usr/sbin/php5enmod mbstring && service apache2 restart',
+  path    => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+  onlyif  => '/usr/sbin/php5query -m | grep -q mbstring',
 }
-
